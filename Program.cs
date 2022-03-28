3966fc6b8a6badc9017d7bc106bac55168fefdaf
hensleyw1 <99474845+hensleyw1@users.noreply.github.com>
2022-03-28 04:36:06 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+String arg1 = ShowCharacter("first statement");
+     Char Letter = arg1[1-1];
+
+double price = CalculateRetail(); 
+double percent = CalculateRetail();
+
+
+static string ShowCharacter(String answer)
+    {
+        Console.WriteLine("Please enter {0}",answer);
+        return Console.ReadLine();
+    }
+        Console.WriteLine($"The letter is {Letter}");
+    
+
+static double CalculateRetail(double money)
+    {
+        Console.WriteLine("What is the {0}", money);
+        return Console.ReadLine();
+        double wholeDollar = ( price *  percent)/100;
+        double total = wholeDollar + price;
+        Console.WriteLine($"The retail price is {total}");
+    }
+)
+
+ 
